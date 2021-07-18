4064cde39b791e03576fdf3eee9e03828d3d229a
WUYUDING <wuyd@ntuc.sg>
2021-07-18 17:08:14 +0800

    update library

--- uikitlibrary/src/main/java/com/yuyi/uikit/popup/Popup.java
+++ uikitlibrary/src/main/java/com/yuyi/uikit/popup/Popup.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.view.Gravity;
 
+import androidx.annotation.ColorInt;
+
 public class Popup {
     public static void topSuccess(Context context, String title) {
         PopupTop popup = new PopupTop(context);
@@ -26,5 +28,9 @@ public class Popup {
         PopupTop popup = new PopupTop(context);
         popup.setTitle(title);
         popup.warning(((Activity) context).getWindow().getDecorView(), Gravity.TOP);
+    }
+    public static void top(Context context, String title, @ColorInt int color){
+        PopupTop popup = new PopupTop(context,title);
+        popup.show(((Activity) context).getWindow().getDecorView(), Gravity.TOP,color);
     }
 }
--- uikitlibrary/src/main/java/com/yuyi/uikit/popup/PopupType.java
+++ uikitlibrary/src/main/java/com/yuyi/uikit/popup/PopupType.java
@@ -2,9 +2,12 @@ package com.yuyi.uikit.popup;
 
 import android.view.View;
 
+import androidx.annotation.ColorInt;
+
 public interface PopupType {
     void error(View parent, int gravity);
     void success(View parent, int gravity);
     void warning(View parent, int gravity);
     void info(View parent, int gravity);
+    void show(View parent, int gravity, @ColorInt int color);
 }
